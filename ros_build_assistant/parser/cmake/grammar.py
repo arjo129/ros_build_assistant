15ca13cf35800cfb33c7accc86206e14cc4b3483
Arjo <arjo129@gmail.com>
2020-11-20 16:14:05 +0800

    Add support for CMake Comments

--- ros_build_assistant/parser/cmake/grammar.py
+++ ros_build_assistant/parser/cmake/grammar.py
@@ -1,5 +1,12 @@
 import enum
 
+"""
+This file contains a parser and code generator for parsing and transforming
+cmake files. (Yes it's hand written, :( )
+
+Grammar is based on: https://cmake.org/cmake/help/latest/manual/cmake-language.7.html#grammar-token-escape-sequence
+"""
+
 class CombinatorState(enum.Enum):
     FINISHED = 0 #We finished parsing the item
     FINISHED_ONE_AFTER = 2 #For cases where look ahead is needed
@@ -175,6 +182,7 @@ class QuotedArgument:
 
 """
 Handles unquoted_argument including both legacy and modern versions
+Note: Finished in this case takes place after a character is read.
 """
 class UnQuotedArgument:
     
@@ -224,3 +232,132 @@ class UnQuotedArgument:
 
     def code_gen(self, data):
         return data["body"]
+
+
+
+"""
+Handles the line comment.
+"""
+class LineComment:
+    def __init__(self):
+        self.state = "EXPECT_HASH"
+        self.body = ""
+
+    def reset(self):
+        self.state = "EXPECT_HASH"
+        self.body = ""
+
+    def next_char(self, next_char):
+        if self.state == "EXPECT_HASH":
+            if next_char == "#":
+                self.state = "EXPECT_BODY"
+                return CombinatorState.IN_PROGRESS, None
+            else:
+                return CombinatorState.ERROR, None
+
+        elif self.state == "EXPECT_BODY":
+            if next_char == "\n":
+                return CombinatorState.FINISHED, {"type": "line_comment", "body": self.body}
+            else:
+                self.body += next_char
+                return CombinatorState.IN_PROGRESS, None
+
+    def code_gen(self, data):
+        return "#" + data["body"] + "\n"
+
+
+"""
+Handles bracket comments
+"""
+class BracketComment:
+
+    def __init__(self):
+        self.state = "EXPECT_HASH"
+        self.bracket_argument = BracketArgument()
+
+    def reset(self):
+        self.state = "EXPECT_HASH"
+        self.bracket_argument = BracketArgument()
+
+    def next_char(self, next_char):
+        if self.state == "EXPECT_HASH":
+            if next_char == "#":
+                self.state = "EXPECT_BRACKET"
+                self.bracket_argument.reset()
+                return CombinatorState.IN_PROGRESS, None
+            else:
+                return CombinatorState.ERROR, None
+        elif self.state == "EXPECT_BRACKET":
+            res, data = self.bracket_argument.next_char(next_char)
+            if res == CombinatorState.ERROR:
+                return CombinatorState.ERROR, None
+            elif res == CombinatorState.FINISHED:
+                return CombinatorState.FINISHED, {"type": "bracket_comment", "body": data}
+            else:
+                return CombinatorState.IN_PROGRESS, None
+
+    def code_gen(self, data):
+        return "#"+self.bracket_argument.code_gen(data["body"])
+
+"""
+Handles Comments
+"""
+class Comment:
+
+    def __init__(self):
+        self.state = "EXPECT_HASH"
+        self.bracket_comment = BracketComment()
+        self.line_comment = LineComment()
+    
+    def next_char(self, next_char):
+
+        if self.state == "EXPECT_HASH":
+            self.bracket_comment.reset()
+            self.line_comment.reset()
+            self.bracket_comment.next_char(next_char)
+            res, _ = self.line_comment.next_char(next_char)
+            if res == CombinatorState.ERROR:
+                return CombinatorState.ERROR, None
+            if res == CombinatorState.IN_PROGRESS:
+                self.state = "DETERMINE_TYPE"
+                return CombinatorState.IN_PROGRESS, None
+
+        elif self.state == "DETERMINE_TYPE":
+            res_bc, _  = self.bracket_comment.next_char(next_char)
+            res_lc, _ = self.line_comment.next_char(next_char)
+            if res_bc == CombinatorState.ERROR:
+                if res_lc == CombinatorState.ERROR:
+                    return CombinatorState.ERROR, None
+                else:
+                    self.state = "LINE_COMMENT"
+                    return CombinatorState.IN_PROGRESS, None
+            else:
+                self.state = "BRACKET_COMMENT"
+                return CombinatorState.IN_PROGRESS, None
+        
+        elif self.state == "LINE_COMMENT":
+            res, data = self.line_comment.next_char(next_char)
+            if res == CombinatorState.FINISHED:
+                return CombinatorState.FINISHED, {"type": "comment", "comment": "line", "body": data}
+            return res, None
+
+        elif self.state == "BRACKET_COMMENT":
+            self.line_comment.next_char(next_char)
+            res, data = self.bracket_comment.next_char(next_char)
+            if res == CombinatorState.FINISHED:
+                return CombinatorState.FINISHED, {"type": "comment", "comment": "bracket", "body": data}
+
+            if res == CombinatorState.ERROR: 
+                #Not in grammar spec just here to allow for malformed comments in older files
+                #Handles malformatted comments like "#[ Some comment".
+                self.state = "LINE_COMMENT"
+                return CombinatorState.IN_PROGRESS, None
+            return res, None
+
+    def code_gen(self, data):
+
+        if data["comment"] == "line":
+            return self.line_comment.code_gen(data["body"])
+
+        elif data["comment"] == "bracket":
+            return self.bracket_comment.code_gen(data["body"])
