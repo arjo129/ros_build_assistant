764e6752b28d55175f1b64d1cd65313683c07bfd
Arjo <arjo129@gmail.com>
2020-11-29 12:35:04 +0800

    More tests

--- tests/cmake/test_command_args.py
+++ tests/cmake/test_command_args.py
@@ -10,9 +10,33 @@ def parse_item(inp):
 
 class TestCommandArguments(unittest.TestCase):
     
-    def test_command_arguments(self):
+    def test_command_arguments_1(self):
         #normal operation
         inp = "( #[[Hello world]] [[some nonsense (]] (A AND B))"
+        res, data = parse_item(inp)
+        self.assertEqual(res, CombinatorState.FINISHED)
+        out = CommandArguments().code_gen(data)
+        self.assertEqual(out, inp)
+
+    def test_command_arguments_2(self):
+        #normal operation
+        inp = "( test )"
+        res, data = parse_item(inp)
+        self.assertEqual(res, CombinatorState.FINISHED)
+        out = CommandArguments().code_gen(data)
+        self.assertEqual(out, inp)
+
+    def test_command_arguments_3(self):
+        #normal operation
+        inp = "( CATKIN_PACKAGES\nroscpp\n\t#octomap_msgs\n\t)"
+        res, data = parse_item(inp)
+        self.assertEqual(res, CombinatorState.FINISHED)
+        out = CommandArguments().code_gen(data)
+        self.assertEqual(out, inp)
+
+    def test_command_arguments_4(self):
+        #normal operation
+        inp = "(CATKIN_PACKAGES\nroscpp\n\t#octomap_msgs\n\t)"
         res, data = parse_item(inp)
         self.assertEqual(res, CombinatorState.FINISHED)
         out = CommandArguments().code_gen(data)
